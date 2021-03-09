e0b6bb9c51f2bfff96d7424bd4dd846ae725a6c2
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2021-03-09 12:32:24 -0600

    allow BigInt for input

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /**
  * Creates an address amount pair for transaction output
  * @param {string}  address
- * @param {int} amountSats
+ * @param {int|BigInt} amountSats
  * @return {Object}
  */
-module.exports=(address,amountSats)=>{
+module.exports=test=(address,amountSats)=>{
     let temp={}
-    temp[address]=(amountSats/100000000).toFixed(8);
+    let amountString=amountSats.toString().padStart(9,'0'); //convert amountSats to a string with at least 9 characters
+    let index=amountString.length-8;                          //where to place the .
+    amountString=amountString.substr(0,index)+"."+amountString.substr(index);//add the .
+    temp[address] = amountString                            //create an object containing value
     return temp;
 }
